08f51bcf6fd9ba6c7abb9070e1a69595eee4829c
phidex <83309249+phidex@users.noreply.github.com>
2021-06-09 17:26:13 +0300

    Add files via upload

---
+++ test/shared/fixtures.ts
@@ -0,0 +1,100 @@
+import { Wallet, Contract } from 'ethers'
+import { Web3Provider } from 'ethers/providers'
+import { deployContract } from 'ethereum-waffle'
+
+import { expandTo18Decimals } from './utilities'
+
+import phidexV2Factory from '@phidex/v2-core/build/phidexV2Factory.json'
+import IphidexV2Pair from '@phidex/v2-core/build/IphidexV2Pair.json'
+
+import ERC20 from '../../build/ERC20.json'
+import WETH9 from '../../build/WETH9.json'
+import phidexV1Exchange from '../../build/phidexV1Exchange.json'
+import phidexV1Factory from '../../build/phidexV1Factory.json'
+import phidexV2Router01 from '../../build/phidexV2Router01.json'
+import phidexV2Migrator from '../../build/phidexV2Migrator.json'
+import phidexV2Router02 from '../../build/phidexV2Router02.json'
+import RouterEventEmitter from '../../build/RouterEventEmitter.json'
+
+const overrides = {
+  gasLimit: 9999999
+}
+
+interface V2Fixture {
+  token0: Contract
+  token1: Contract
+  WETH: Contract
+  WETHPartner: Contract
+  factoryV1: Contract
+  factoryV2: Contract
+  router01: Contract
+  router02: Contract
+  routerEventEmitter: Contract
+  router: Contract
+  migrator: Contract
+  WETHExchangeV1: Contract
+  pair: Contract
+  WETHPair: Contract
+}
+
+export async function v2Fixture(provider: Web3Provider, [wallet]: Wallet[]): Promise<V2Fixture> {
+  // deploy tokens
+  const tokenA = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)])
+  const tokenB = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)])
+  const WETH = await deployContract(wallet, WETH9)
+  const WETHPartner = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)])
+
+  // deploy V1
+  const factoryV1 = await deployContract(wallet, phidexV1Factory, [])
+  await factoryV1.initializeFactory((await deployContract(wallet, phidexV1Exchange, [])).address)
+
+  // deploy V2
+  const factoryV2 = await deployContract(wallet, phidexV2Factory, [wallet.address])
+
+  // deploy routers
+  const router01 = await deployContract(wallet, phidexV2Router01, [factoryV2.address, WETH.address], overrides)
+  const router02 = await deployContract(wallet, phidexV2Router02, [factoryV2.address, WETH.address], overrides)
+
+  // event emitter for testing
+  const routerEventEmitter = await deployContract(wallet, RouterEventEmitter, [])
+
+  // deploy migrator
+  const migrator = await deployContract(wallet, phidexV2Migrator, [factoryV1.address, router01.address], overrides)
+
+  // initialize V1
+  await factoryV1.createExchange(WETHPartner.address, overrides)
+  const WETHExchangeV1Address = await factoryV1.getExchange(WETHPartner.address)
+  const WETHExchangeV1 = new Contract(WETHExchangeV1Address, JSON.stringify(phidexV1Exchange.abi), provider).connect(
+    wallet
+  )
+
+  // initialize V2
+  await factoryV2.createPair(tokenA.address, tokenB.address)
+  const pairAddress = await factoryV2.getPair(tokenA.address, tokenB.address)
+  const pair = new Contract(pairAddress, JSON.stringify(IphidexV2Pair.abi), provider).connect(wallet)
+
+  const token0Address = await pair.token0()
+  const token0 = tokenA.address === token0Address ? tokenA : tokenB
+  const token1 = tokenA.address === token0Address ? tokenB : tokenA
+
+  await factoryV2.createPair(WETH.address, WETHPartner.address)
+  const WETHPairAddress = await factoryV2.getPair(WETH.address, WETHPartner.address)
+  const WETHPair = new Contract(WETHPairAddress, JSON.stringify(IphidexV2Pair.abi), provider).connect(wallet)
+
+  return {
+    token0,
+    token1,
+    WETH,
+    WETHPartner,
+    factoryV1,
+    factoryV2,
+    router01,
+    router02,
+    router: router02, // the default router, 01 had a minor bug
+    routerEventEmitter,
+    migrator,
+    WETHExchangeV1,
+    pair,
+    WETHPair
+  }
+}
